e6ba5b31247fbcd2b69b090199e8de909029c177
Victor Gaugler <victor.gaugler@hotmail.fr>
2021-03-19 09:31:43 +0100

    home page presque fini

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+// scroll menu
 
 	window.addEventListener("scroll", function(){
 		var header = document.querySelector("header");
 		header.classList.toggle("sticky", window.scrollY > 0);
 	})
 
+// toggle menu 
 
 function toggleMenu(){
 	var menuToggle = document.querySelector('.toggle');
@@ -12,6 +14,8 @@ function toggleMenu(){
 	menu.classList.toggle('active');
 
 }
+
+// slide photo
 
 var counter = 1;
     setInterval(function(){
@@ -49,10 +53,83 @@ sr.reveal('.work__img',{interval: 200});
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
+// candy mode
 
-function myFunction() {
-  var element = document.body;
-  element.classList.toggle("candy-mode");
-  var element = document.querySelector('.acceuilBox_box1');
-  element.classList.toggle("candy-mode");
-}
+
+ 
+  
+  
+const subtitle = document.querySelector('h3.subtitle');
+const nav = document.querySelector('li.anav');
+const menu = document.getElementById('head');
+const btncandy = document.querySelector('.btnmode1');
+const btntacos = document.querySelector('.btnmode2');
+const banner = document.getElementById("banner");
+const acceuilBox = document.querySelector('.acceuilBox_box1');
+const info = document.querySelector('.defil');
+let slide = document.querySelector('.imgslide');
+let slide2 = document.querySelector('.imgslide2');
+const comm1 = document.querySelector('.commclient');
+const comm2 = document.querySelector('.commclient2');
+
+
+btncandy.addEventListener('click', function(){
+  banner.classList.toggle("candy-mode");
+  acceuilBox.classList.toggle("candy-mode");
+  nav.classList.toggle("candy-mode");
+  menu.classList.toggle("candy-mode");
+});
+
+btntacos.addEventListener('click', function(){
+  banner.classList.toggle("tacos-mode");
+  acceuilBox.classList.toggle("tacos-mode");
+  menu.classList.toggle("tacos-mode");
+  subtitle.classList.toggle("tacos-mode");
+  if (subtitle.className === "subtitle") {
+		subtitle.innerHTML = "Patisserie Boulangerie";
+	} else {
+		subtitle.innerHTML = " Tacosserie";
+	};
+  info.classList.toggle("tacos-mode");
+  if (info.className === "infoBloc") {
+		info.innerHTML = "Adressse , numéro de télèphone, ouvert 7/7 même après le couvre feu";
+	} else {
+		info.innerHTML = " 'Livraison de tacos moisi et pas moisi , ça dépend quel jour 24/24'";
+	};
+  slide.classList.toggle("tacos-mode");
+  if (slide.className === "imgslide"){
+    slide.src = 'images/slide1.jpg';
+  } else {
+    slide.src = "images/tac1.jpg";
+  }
+  slide2.classList.toggle("tacos-mode");
+  if (slide2.className === "imgslide2"){
+    slide2.src = 'images/slide2.jpg';
+  } else {
+    slide2.src = "images/tac2.jpg";
+  }
+  comm1.classList.toggle("tacos-mode");
+  if (comm1.className === "commclient"){
+    comm1.innerHTML = "C'était super bon,</br> merci pour tout, j'adore !";
+  }
+  else {
+    comm1.innerHTML = "J'adore les tacos </br> surtout quand ils sont moisis !";
+  }
+  comm2.classList.toggle("tacos-mode");
+  if (comm2.className === "commclient2"){
+    comm2.innerHTML = "C'était succulent ,</br> quelles saveurs, on en redemande !";
+  }
+  else {
+    comm2.innerHTML = "J'adore le PHP </br> surtout avec de la sauce Samourai !";
+  }
+
+});
+
+ 
+
+
+
+
+// badge redirection
+
+
