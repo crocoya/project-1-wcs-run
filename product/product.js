7d08cb50ea7f0d49a64d9157a64d99ecc6336aea
Yassine <yassine.tababi@outlook.com>
2021-03-23 09:34:31 +0100

    9h34

--- product/product.js
+++ product/product.js
@@ -14,3 +14,15 @@ menuToggle.classList.toggle('active');
 menu.classList.toggle('active');
 
 }
+
+var slide = new Array("foret-peuplier.jpg", "paysage-montagne.jpg", "chemin-automne.jpg", "prairie-alpes.jpg");
+var numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
+}
