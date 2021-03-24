9ba46a650e6489a5c8785e409b50f3557724915b
Crocoya <yassine.tababi@outlook.com>
2021-03-24 11:38:07 +0100

    11h37

--- product/product.js
+++ product/product.js
@@ -27,7 +27,7 @@ function ChangeSlide(sens) {
     document.getElementById("slide1").src = slide[numero];
 }
 
-var slide2 = new Array("images/pâtisserie/macaron-blanc.jpg", "images/pâtisserie/macaron.jpg", "images/pâtisserie/mirtille.jpg");
+var slide2 = new Array("images/viennoiseries/croissant-sucre.jpg", "images/viennoiseries/croissant-panier.jpg", "images/viennoiseries/pain-au-chocolat.jpg");
 var numero2 = 0;
 
 function ChangeSlide2(sens) {
@@ -37,4 +37,16 @@ function ChangeSlide2(sens) {
     if (numero2 > slide2.length - 1)
         numero2 = 0;
     document.getElementById("slide2").src = slide2[numero2];
+}
+
+var slide3 = new Array("images/snacking/salade.jpg", "images/snacking/croissant-salade.jpg", "images/snacking/croissant-sandwitch.jpg");
+var numero3 = 0;
+
+function ChangeSlide3(sens) {
+    numero3 = numero3 + sens;
+    if (numero3 < 0)
+        numero3 = slide2.length - 1;
+    if (numero3 > slide2.length - 1)
+        numero3 = 0;
+    document.getElementById("slide3").src = slide3[numero3];
 }
